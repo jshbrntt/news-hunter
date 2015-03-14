b0258d0590c63d00989939af7dd94d1e3932c890
Joshua Barnett <jbacontrol@gmail.com>
2015-03-14 20:33:15 +0000

    Adding default gulp task, and fixing clean task.

--- gulpfile.js
+++ gulpfile.js
@@ -10,10 +10,10 @@ var uglify = require('gulp-uglify');
 var wiredep = require('wiredep').stream;
 
 gulp.task('clean', function (cb) {
-    del(['build/**/*'], cb);
+    del(['build/*.js', 'build/*.css', 'build/*.html'], cb);
 });
 
-gulp.task('scripts', function () {
+gulp.task('scripts', ['clean'], function () {
     var browserified = transform(function (filename) {
         var b = browserify({
             entries: filename,
@@ -31,19 +31,25 @@ gulp.task('scripts', function () {
         .pipe(gulp.dest('./build'));
 });
 
-gulp.task('styles', function () {
+gulp.task('styles', ['clean'], function () {
     return gulp.src('./app/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('./build'));
 });
 
-gulp.task('markup', function () {
+gulp.task('markup', ['clean'], function () {
     return gulp.src('./app/*.html')
         .pipe(gulp.dest('./build'));
 });
 
-gulp.task('bower', function () {
+gulp.task('bower', ['markup'], function () {
     return gulp.src('./build/index.html')
         .pipe(wiredep())
         .pipe(gulp.dest('./build'));
 });
+
+gulp.task('default', [
+    'scripts',
+    'styles',
+    'bower'
+]);
