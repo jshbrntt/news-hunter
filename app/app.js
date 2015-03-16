8fce197888dcf5f417ff7c52ca07002b666333a9
Joshua Barnett <jbacontrol@gmail.com>
2015-03-16 23:49:29 +0000

    Feature complete.

--- app/app.js
+++ app/app.js
@@ -3,65 +3,96 @@ var angular = require('angular');
 
 (function () {
 
-    var app = angular.module('newsHunter', []);
+    var app = angular.module('newsHunter', ['ngRoute']);
 
-    var getRandomCategory = function (categories) {
-        if (angular.isArray(categories)) {
-            return categories[Math.floor(Math.random() * categories.length)];
-        }
-        return null;
-    };
-
-    app.controller('SidebarController', function () {
-
-        this.categories = [
-            'africa',
-            'asia',
-            'celebrity',
-            'europe',
-            'fashion',
-            'health',
-            'movies',
-            'music',
-            'politics',
-            'science',
-            'sports',
-            'stocks',
-            'tech',
-            'topstories',
-            'travel',
-            'tv',
-            'us',
-            'weather',
-            'world'
-        ];
-
-    });
-
-    app.controller('NewsController', ['$http', function ($http) {
-
-        var news = this;
-
-        this.category = getRandomCategory(this.categories);
-        this.url = 'http://rss.news.yahoo.com/rss/' + this.category;
-
-        $http({
-            url: 'https://query.yahooapis.com/v1/public/yql',
-            method: 'GET',
-            params: {
-                'q': "select * from rss where url='" + this.url + "'",
-                'format': "json"
-            }
-        }).success(function (data) {
-            console.log(data);
-//            news.stories = data.query.results.item;
+    // Filters
+    app.filter('rawHtml', ['$sce', function ($sce) {
+        return function (val) {
+            return $sce.trustAsHtml(val);
+        };
+    }]);
+    
+    // Config
+    app.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.
+        when('/:category', {
+            templateUrl: 'news.html',
+            controller: 'NewsCtrl'
+        }).
+        otherwise({
+            redirectTo: '/topstories'
         });
     }]);
 
-    app.filter('renderHTMLCorrectly', function ($sce) {
-        return function (stringToParse) {
-            return $sce.trustAsHtml(stringToParse);
+    // Directives
+    app.directive('sidebar', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'sidebar.html'
         };
     });
+
+    app.factory('YahooNewsService', function ($http) {
+
+        function _getNewsForCategory(category) {
+            return $http({
+                url: 'https://query.yahooapis.com/v1/public/yql',
+                method: 'GET',
+                params: {
+                    'q': "select * from rss where url='http://rss.news.yahoo.com/rss/" + category + "'",
+                    'format': "json"
+                }
+            }).
+            then(function (response) {
+                return response.data;
+            });
+        }
+
+        return {
+            getNewsForCategory: _getNewsForCategory
+        };
+
+    });
+
+    // Services
+    app.factory('CategoryService', function () {
+        return {
+            list: [
+                'africa',
+                'asia',
+                'celebrity',
+                'europe',
+                'fashion',
+                'health',
+                'movies',
+                'music',
+                'politics',
+                'science',
+                'sports',
+                'stocks',
+                'tech',
+                'topstories',
+                'travel',
+                'tv',
+                'us',
+                'weather',
+                'world'
+            ]
+        };
+    });
+
+    // Controllers
+    app.controller('SidebarCtrl', ['CategoryService', function (CategoryService) {
+        this.categories = CategoryService.list;
+    }]);
+
+    app.controller('NewsCtrl', ['$scope', '$routeParams', 'YahooNewsService', function ($scope, $routeParams, YahooNewsService) {
+        this.category = $routeParams.category;
+        var ctrl = this;
+        YahooNewsService.getNewsForCategory($routeParams.category).
+        then(function (data) {
+            ctrl.news = data.query.results.item;
+        });
+    }]);
 
 })();
